900f376b87608c6c8bac104dcaa2b429d4a78e8d
extincto <yannick.jammes@epsi.fr>
2019-01-08 09:34:11 +0100

    header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+import { WebcamInitError, WebcamImage, WebcamUtil } from 'ngx-webcam';
+import { Subject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  private trigger: Subject<void> = new Subject<void>();
+  public webcamImage: WebcamImage = null;
 
-  constructor() { }
+  constructor(private home: HomeComponent) { }
 
   ngOnInit() {
+    this.home.triggerSnapshot();
   }
 
+  public triggerSnapshot(): void {
+    this.trigger.next();
+  }
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+  public handleImage(webcamImage: WebcamImage): void {
+    // tslint:disable-next-line:no-console
+    console.info('received webcam image', webcamImage);
+    this.webcamImage = webcamImage;
+  }
 }
